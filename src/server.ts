6fa637726b9b68bb0f2ce1656bc00f75746dc088
JeffersonCorreiax <correia_jefferson@outlook.com>
2023-05-09 01:21:56 -0300

    correção

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,4 @@ export { app, prisma };
 app.listen({
     host: '0.0.0.0',
     port: process.env.PORT ? Number(process.env.PORT) : 3333,
-}).then(() => {
-    console.log('HTTP Server Running')
-} )
+})
